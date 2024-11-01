5c90904230dadc96552842adc65447dc743a2ee9
Ken Henkel <kenhenkel95@gmail.com>
2024-11-01 11:09:50 -0400

    Refactor code to compare choices into its own func
    Refactored the code to compare the human and computer choices into its
    own compareChoices function accepting humanChoice and computerChoice as
    arguments. Moved humanScore and computerScore back into the global scope
    to accommodate this

--- javascript.js
+++ javascript.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice(){
     let computerNumber = Math.floor(Math.random() * 3);
 
@@ -32,52 +35,56 @@ function whoWon(humanScore, computerScore){
     }
 }
 
+function compareChoice(humanChoice, computerChoice){
+    while(humanChoice === computerChoice){
+        console.log(`It's a draw. You both chose ${humanChoice}. Try again.`);
+        humanChoice = getHumanChoice();
+        computerChoice = getComputerChoice();
+    }
+    if(humanChoice === "rock"){
+        if(computerChoice === "paper"){
+            console.log("You lose! Paper beats rock");
+            computerScore++;
+        }
+        else if(computerChoice === "scissors"){
+            console.log("You win! rock beats scissors!");
+            humanScore++;
+        }
+    }
+
+    if(humanChoice === "paper"){
+        if(computerChoice === "rock"){
+            console.log("You win! Paper beats rock");
+            humanScore++;
+        }
+        else if(computerChoice === "scissors"){
+            console.log("You lose! scissors beats paper!");
+            computerScore++;
+        }
+    }
+
+    if(humanChoice === "scissors"){
+        if(computerChoice === "paper"){
+            console.log("You win! scissors beats paper");
+            humanScore++;
+        }
+        else if(computerChoice === "rock"){
+            console.log("You lose! rock beats scissors!");
+            computerScore++;
+        }
+    }
+}
+
+
 function playGame(){
-    let humanScore = 0;
-    let computerScore = 0;
+    
 
     function playRound(){
 
         let humanChoice = getHumanChoice();
         let computerChoice = getComputerChoice();
-
-        while(humanChoice === computerChoice){
-            console.log(`It's a draw. You both chose ${humanChoice}. Try again.`);
-            humanChoice = getHumanChoice();
-            computerChoice = getComputerChoice();
-        }
-        if(humanChoice === "rock"){
-            if(computerChoice === "paper"){
-                console.log("You lose! Paper beats rock");
-                computerScore++;
-            }
-            else if(computerChoice === "scissors"){
-                console.log("You win! rock beats scissors!");
-                humanScore++;
-            }
-        }
-    
-        if(humanChoice === "paper"){
-            if(computerChoice === "rock"){
-                console.log("You win! Paper beats rock");
-                humanScore++;
-            }
-            else if(computerChoice === "scissors"){
-                console.log("You lose! scissors beats paper!");
-                computerScore++;
-            }
-        }
-    
-        if(humanChoice === "scissors"){
-            if(computerChoice === "paper"){
-                console.log("You win! scissors beats paper");
-                humanScore++;
-            }
-            else if(computerChoice === "rock"){
-                console.log("You lose! rock beats scissors!");
-                computerScore++;
-            }
-        }
+        compareChoice(humanChoice, computerChoice);
+        
     }
     
     for(i = 0; i < 5; i++){
